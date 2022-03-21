c857c5f0b8c97e11e8da74d938a3824964eb144a
Furkan394 <furkanncakiroglu@gmail.com>
2022-03-21 18:45:13 +0300

    first commit

---
+++ web-customer-tracker/src/com/emre/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,20 @@
+package com.emre.springdemo.dao;
+
+import java.util.List;
+
+import com.emre.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer customer);
+
+	public Customer getCustomer(int id);
+
+	public void deleteCustomer(int id);
+
+	public List<Customer> searchCustomers(String theSearchName);
+	
+
+}
